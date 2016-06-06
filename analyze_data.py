87b1b2768c7d8b85cd01efdf68e5ff8b3eddf7a5
Peter Washington <peter.y.washington@gmail.com>
2016-06-05 18:54:01 -0700

    Added initial spreadsheet for data collection

---
+++ analyze_data.py
@@ -0,0 +1,28 @@
+
+### Script to create plots based on study data.
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import glob
+import time
+import datetime
+import collections
+import json
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+	main()
